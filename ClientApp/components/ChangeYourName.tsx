d154d997eac0587bf4c906b760bf4e793800ff0b
prescottlewis707 <prescottlewis707@gmail.com>
2018-03-22 19:31:08 +0000

    first commit

---
+++ ClientApp/components/ChangeYourName.tsx
@@ -0,0 +1,112 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { InputForm } from './InputForm';
+
+// Step 1: Add an interface 
+interface CounterState {
+    name: string;
+}
+
+interface FetcUsersDataState {
+    users: User[];
+    loading: boolean;
+}
+
+interface User {
+    name: string;
+    surname: string;
+}
+
+interface UsersState {
+    users: Array<User>;
+    newUser: User;
+    loading: boolean;
+}
+
+export class ChangeYourName extends React.Component<RouteComponentProps<{}>, UsersState> {
+    constructor() {
+        super();
+
+        // Step 2: Set the initial state 
+        this.state = {
+            users: [],
+            newUser: {
+                name: '',
+                surname: '',
+            },
+            loading: false,
+        };
+
+        // Step 4: Declare the event handlers on constructor
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        fetch('api/users/')
+            .then(response => response.json() as Promise<Array<User>>)
+            .then(data => {
+                this.setState({ users: data, loading: false });
+            });
+    }
+
+    public render() {
+
+        // Step 3 Add the for code
+        let users: Array<User> = this.state.users;
+        return <div>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Surname</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) =>
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.surname}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            <form onSubmit={this.handleSubmit}>
+                <InputForm name='name' value='' onValueChange={this.handleChange} />
+                <InputForm name='surname' value='' onValueChange={this.handleChange} />
+                <button>
+                    Submit
+            </button>
+            </form>
+        </div>;
+    }
+
+    // Step 4
+    handleChange(input: any) {
+        if (input.name !== '') {
+            let userToChange = this.state.newUser;
+
+            if (input.name === "surname") {
+                userToChange.surname = input.value;
+            } else {
+                userToChange.name = input.value;
+            }
+
+            this.setState({ newUser: userToChange });
+        }
+    }
+
+    // 
+    handleSubmit(event: any) {
+        console.log(this.state);
+        const myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+        return fetch('api/users/', {
+            method: 'POST',
+            headers: myHeaders,
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify(this.state.newUser)
+        }).then((response) => {
+            return response.json();
+        });
+    }
+}
---
+++ ClientApp/components/InputForm.tsx
@@ -0,0 +1,74 @@
+﻿import * as React from "react";
+import { PropTypes } from "react";
+
+//Step 2: Initialise the props with an interface
+interface IInputFormComponentProps {
+    value?: string;
+    name?: string;
+    onValueChange: (value: any) => void;
+}
+
+
+//Step 3: Declare the state interfaces
+interface IInputFormComponentState {
+    value: string;
+    name: string;
+}
+
+const initialState: IInputFormComponentState = {
+    value: '',
+    name: ''
+};
+
+export class InputForm extends React.Component<IInputFormComponentProps, IInputFormComponentState> {
+
+    // Step 1: Our component has a properties
+    // A string and a function that returns the value 
+    // when the value changes
+    static propTypes = {
+        name: PropTypes.string,
+        value: PropTypes.string,
+        onValueChange: PropTypes.func.isRequired
+    };
+
+    static defaultProps = {
+        value: "",
+        name: "",
+    };
+
+    constructor(props: IInputFormComponentProps) {
+        super(props);
+
+        // Step 3
+        this.state = initialState;
+
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidUpdate(prevProps: IInputFormComponentProps, prevState: IInputFormComponentState): void {
+        //Step 4: When the components update add the following code 
+
+        if (this.props.onValueChange) {
+            this.props.onValueChange(this.state);
+        }
+    }
+
+    public render(): JSX.Element {
+        //Step : Add the render method for the field
+        return (
+            <div>
+                <label>
+                    {this.props.name}
+                    <input type="text" name={this.props.name} value={this.state.value} onChange={this.handleChange} />
+                </label>
+            </div>
+        );
+    }
+
+    private handleChange(event: any): void {
+        this.setState({ name: event.target.name, value: event.target.value });
+    }
+}
+
+export default InputForm;
